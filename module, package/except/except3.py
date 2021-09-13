ce14c3b9173d1dda27cf742ee63cd19a60271fd4
유하람 <w2036@e-mirim.hs.kr>
2021-09-13 13:40:07 +0900

    예외처리 연습

---
+++ module, package/except/except3.py
@@ -0,0 +1,26 @@
+'''
+    
+Java
+
+try {
+    // 예외 발생할 것 같은 코드
+} catch(IndexOutOfRangeException e) {
+    // 예외 처리하는 코드
+} finally {
+    //무조건 실행되어야 하는 코드
+
+
+Python
+
+try:
+    # 예외가 발생할 것 같은 코드
+except IndexError as e: # 특정 예외
+    # 예외 처리하는 코드
+except:
+    # 예외 처리하는 코드
+else:
+    # 예외가 발생하지 않았을 때 실행하는 코드
+finally:
+    # 꼭 실행해야 하는 코드
+
+'''
