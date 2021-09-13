522e05b037a28b3d72a53baa56d3112aa54b3a6c
유하람 <w2036@e-mirim.hs.kr>
2021-09-13 13:55:35 +0900

    InvalidLengthError

--- module, package/baseball.py
+++ module, package/baseball.py
@@ -1,9 +1,20 @@
+from baseball_game_Engine import make_answer, check
+
 answer = make_answer()
-print(answer)
 
 while True: # 무한 반복
     # 숫자 묻기
     guess = input('숫자 세자리를 입력해주세요 : ')
+
+    # 숫자인지 아닌지 확인하기
+    try:
+        guess_int = int(guess)
+    except ValueError as e:
+        print('숫자 세자리를 입력하세요 : ')
+        continue
+
+    if len(guess) != len(answer):
+        raise InvalidLengthError('정답의 길이와 다른 숫자를 입력했습니다.')
 
     # strike, ball 판정하기
     strike, ball = check(guess, answer)
