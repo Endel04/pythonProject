5fa7ca8f0ce591f5142bf3cf60c6f31f169f7c12
유하람 <w2036@e-mirim.hs.kr>
2021-09-27 14:11:09 +0900

    save text(utf-8), '\n'

--- module, package/file handler/save_text.py
+++ module, package/file handler/save_text.py
@@ -1,8 +1,11 @@
-f = open('text.txt', 'w')
+f = open('text.txt', 'w', encoding='utf-8')
 
-f.write('재밌게 본 애니 ')
-f.write('주술회전 ')
-f.write('귀멸의 칼날 ')
+f.write('재밌게 본 애니')
+f.write('\n')
+f.write('주술회전')
+f.write('\n')
+f.write('귀멸의 칼날')
+f.write('\n')
 f.write('코바야시네 메이드래곤')
 
 f.close()
