588a315d7bb3c593bd67bc3939d2787ea85103eb
유하람 <w2036@e-mirim.hs.kr>
2021-10-08 15:04:14 +0900

    person, save_object, load_object

---
+++ module, package/file handler/save_object.py
@@ -0,0 +1,23 @@
+import pickle
+
+from person import Person
+
+# 객체 생성하기
+번호15 = Person('이재령', '하얀색')
+번호17 = Person('임사랑', '분홍색')
+
+# 리스트 만들기
+절친 = [번호15, 번호17]
+# print(절친)
+
+# for 친구 in 절친:
+#     print(친구)
+
+# 저장하기
+# 1. 객체 한 개 저장
+with open('object.bin', 'wb') as f:
+    pickle.dump(번호15, f)
+
+# 2. 객체 여러 개 저장
+with open('objects.bin', 'wb') as f:
+    pickle.dump(절친, f)
