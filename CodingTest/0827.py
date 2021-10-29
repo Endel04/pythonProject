0334ef1ebbc877eec5b5692b030661808298c88c
유하람 <w2036@e-mirim.hs.kr>
2021-10-29 14:25:22 +0900

    All Commit

--- CodingTest/0827.py
+++ CodingTest/0827.py
@@ -104,3 +104,45 @@ print(string.split('/'))
 data = [2, 4, 3, 1, 5, 10, 9]
 data = sorted(data, reverse=False)
 print(data)
+
+# 18)
+
+time = input("현재 시간 : ")
+
+if time[-2:] == "00":
+    print("정각 입니다.")
+else:
+    print("정각이 아닙니다.")
+
+# 19)
+
+리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+
+for i in 리스트:
+    r = i.split('.')
+    if r[1] == 'h':
+        print(i)
+
+# 20)
+
+for num in range(10):
+    print(num / 10)
+
+# 21)
+
+apart = [[101, 102], [201, 202], [301, 302]]
+
+for row in apart:
+    for col in row:
+        print(col, "호")
+
+# 22)
+
+list = [3, 4, 5, 6, 7, 8]
+
+def pickup_even(list):
+    result = []
+    for l in list:
+        if list % 2 == 0:
+            result.append(list)
+    return result
